8829ab7a7a805bd19418f95cb2933a502aaf9b95
Oliver Steffen <olisteffen@posteo.de>
2017-01-19 22:42:30 +0100

    added the darkness and the list command

--- guitars.pl
+++ guitars.pl
@@ -10,11 +10,16 @@ while(my $line = <DATA>) {
   my($tag, $ythash, $start, $length) = split(/\s+/,$line);
   $data{$tag} = [$ythash, $start, $length];
 }
-my $cmdline = sprintf("mpv --no-video ytdl://%s --start %.2f --length %.2f", @{$data{$TAG}});
-system($cmdline);
+if($TAG eq "list") {
+	print "$_\n" for keys %data;
+} else {
+	my $cmdline = sprintf("mpv --no-video ytdl://%s --start %.2f --length %.2f", @{$data{$TAG}});
+	system($cmdline);
+}
 
 
 __DATA__
 Ramstein 	W3q8Od5qJio	16.5  5
 TeenSpirit	hTWKbfoikeg    9  5
 ChopSuey	CSvFpBOe8eY   30  5
+TheDarkness tKjZuykKY1I   11  8
